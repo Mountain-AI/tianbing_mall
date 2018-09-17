cbc51a777763701538d9192e40c35ee8f569840b
devezhang <devezhang@gmail.com>
2018-09-17 10:49:51 +0800

    立项&基本配置

---
+++ tianbing_mall/tianbing_mall/apps/users/models.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    """用户模型"""
+    # 定义表字段
+    mobile = models.CharField(max_length=11, unique=True, verbose_name="手机号")
+
+    # 定义表信息
+    class Meta:
+        db_table = "tb_users"  # 表名
+        verbose_name = "用户"
+        verbose_name_plural = verbose_name  # plural复数
+
+    # 在django中,定义完模型后第一次迁移需要配置文件设置AUTH_USER_MODEL='users.User'
+        # 中间省略了models,是因为添加了导包路径;why?
