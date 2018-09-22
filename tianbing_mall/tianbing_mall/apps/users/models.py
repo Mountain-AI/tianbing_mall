51673beecc1ce438fbe4f1c8a0e9d0b7b18a6eab
devezhang <devezhang@gmail.com>
2018-09-22 16:04:41 +0800

    用户模型增加邮箱是否验证字段

--- tianbing_mall/tianbing_mall/apps/users/models.py
+++ tianbing_mall/tianbing_mall/apps/users/models.py
@@ -8,6 +8,8 @@ class User(AbstractUser):
     """用户模型:继承django封装好的abstractuser抽象用户类,再次基础上增加mobile字段"""
     # 定义表字段
     mobile = models.CharField(max_length=11, unique=True, verbose_name="手机号")
+    # 补充邮箱是否激活字段:实现个人中心
+    email_active = models.BooleanField(default=False, verbose_name="邮箱验证状态")
 
     # 定义表信息
     class Meta:
